4b7823441f5e6fb78898ee07719ef1a68434aa92
Toyes <toyeschawla02@gmail.com>
2026-02-12 02:01:02 +0530

    changed the CTA section

--- src/pages/Home/Hero-Section.jsx
+++ src/pages/Home/Hero-Section.jsx
@@ -4,7 +4,7 @@ function HeroSection()
     return (
         <>
             <section className="hero-section">
-                <h1 className="hero-heading"><span className="highlight highlight-secondary">Campus delivery,</span> powered by students <br className="hero-heading-break" />for students 📍</h1>
+                <h1 className="hero-heading"><span className="highlight highlight-secondary">Campus delivery,</span> powered by students for students </h1>
                 <p className="hero-paragraph">A smart peer-to-peer assistance network that connects hostel students with nearby helpers for urgent academic and daily needs</p>
                 <ul className="hero-btn-container">
                     <li><Link to="/register" className="btn btn-primary" >Get started</Link></li>
